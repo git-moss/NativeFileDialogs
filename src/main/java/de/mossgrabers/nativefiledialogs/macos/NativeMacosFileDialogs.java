b973a1724ba3e8c12e2e084c2da9d3f983873b62
Jürgen Moßgraber <mos@moss-mac-mini.fritz.box>
2020-12-02 00:10:00 +0100

    Implemented pre-selected file for Macos.

--- src/main/java/de/mossgrabers/nativefiledialogs/macos/NativeMacosFileDialogs.java
+++ src/main/java/de/mossgrabers/nativefiledialogs/macos/NativeMacosFileDialogs.java
@@ -23,7 +23,6 @@ public class NativeMacosFileDialogs extends AbstractNativeFileDialogs
     private static final String SAVE_START   = "tell application \"Finder\"\nActivate\ntry\nPOSIX path of ( choose file name ";
     private static final String END          = ")\non error number -128\nend try\nend tell";
 
-
     /**
      * Creates a new file dialog instance with the initial directory.
      *
@@ -40,7 +39,7 @@ public class NativeMacosFileDialogs extends AbstractNativeFileDialogs
     public File selectFile (final String title, final FileFilter... filters) throws IOException
     {
         final StringBuilder applescriptCommand = new StringBuilder (OPEN_START);
-        this.addTitleAndDirectory (applescriptCommand, title);
+        this.addTitleAndDirectory (applescriptCommand, title, false, false);
 
         if (filters.length > 0)
         {
@@ -69,7 +68,7 @@ public class NativeMacosFileDialogs extends AbstractNativeFileDialogs
     public File selectNewFile (final String title, final FileFilter... filters) throws IOException
     {
         final StringBuilder applescriptCommand = new StringBuilder (SAVE_START);
-        this.addTitleAndDirectory (applescriptCommand, title);
+        this.addTitleAndDirectory (applescriptCommand, title, false, true);
         applescriptCommand.append (END);
 
         final String filename = runApplescript (applescriptCommand.toString ());
@@ -82,7 +81,7 @@ public class NativeMacosFileDialogs extends AbstractNativeFileDialogs
     public File selectFolder (final String title) throws IOException
     {
         final StringBuilder applescriptCommand = new StringBuilder (FOLDER_START);
-        this.addTitleAndDirectory (applescriptCommand, title);
+        this.addTitleAndDirectory (applescriptCommand, title, true, false);
         applescriptCommand.append (END);
 
         final String filename = runApplescript (applescriptCommand.toString ());
@@ -95,14 +94,23 @@ public class NativeMacosFileDialogs extends AbstractNativeFileDialogs
      *
      * @param applescriptCommand The apple script
      * @param title The title
+     * @param onlyFolder True if only the folder should be added as pre-selection
+     * @param addFilename True to add the name as pre-selection
      */
-    private void addTitleAndDirectory (final StringBuilder applescriptCommand, final String title)
+    private void addTitleAndDirectory (final StringBuilder applescriptCommand, final String title, final boolean onlyFolder, final boolean addFilename)
     {
         if (title != null && !title.isEmpty ())
             applescriptCommand.append (String.format ("with prompt \"%s\" ", title));
 
-        if (this.currentDirectory != null)
-            applescriptCommand.append (String.format ("default location \"%s\" ", this.currentDirectory.getAbsolutePath ()));
+        if (this.currentDirectory == null)
+            return;
+        File dir = this.currentDirectory;
+        if (onlyFolder)
+            dir = this.currentDirectory.isDirectory () ? this.currentDirectory : this.currentDirectory.getParentFile ();
+        applescriptCommand.append (String.format ("default location \"%s\" ", dir.getAbsolutePath ()));
+
+        if (addFilename && this.currentDirectory.isFile ())
+            applescriptCommand.append (String.format ("default name \"%s\" ", this.currentDirectory.getName ()));
     }
 
 
